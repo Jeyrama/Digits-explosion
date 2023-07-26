1c7e136aa8a7f9bce30eed03b046cf30ea3ff841
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-26 09:25:41 -0700

    Update Digits-explosion.js

--- Digits-explosion.js
+++ Digits-explosion.js
@@ -10,10 +10,20 @@ Example:
 */
 
 
-// Solutiion
+// Solution
 
 function explode(s) {
   return s.split("").map((e) => e.repeat(+e)).join("");
 }
 
 // or
+
+function explode(s) {
+  let newStr = "";
+  for (let i = 0; i < s.length; i++) {
+    for (let num = s[i]; num > 0; num--) {
+      newStr += s[i]
+    }
+  }
+  return newStr
+}
